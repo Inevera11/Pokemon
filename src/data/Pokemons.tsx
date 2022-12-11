bab82e1573b0d1587352097bf51c08df129b1df1
Inevera11 <zuzannabozek2002@gmail.com>
2022-12-11 23:55:29 +0100

    Pokemon Data

--- src/data/Pokemons.tsx
+++ src/data/Pokemons.tsx
@@ -1,21 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { Pokemon } from "../types/interfaces";
 const PokemonData = () => {
-  const [pokemons, setPokemons] = useState<Pokemon[]>();
+  useEffect(() => {
+    fetchData()
+      .then((data) => {
+        setPokemons(data.results.slice(0, 10));
+        console.log("got it", data.results.slice(0, 10));
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
+
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
   const fetchData = async () => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0").then(
-      (response) =>
-        response
-          .json()
-          .then((res) => setPokemons(res))
-          .catch((err) => {
-            console.log(err.message);
-          })
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
     );
-    console.log(pokemons);
+
+    if (response.status !== 200) {
+      throw new Error("cannot fetch the data");
+    }
+
+    const data = await response.json();
+    return data;
   };
-  return fetchData();
+
+  console.log("pokemons", pokemons);
+  return pokemons;
 };
 
 export default PokemonData;
