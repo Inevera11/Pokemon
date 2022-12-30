cfe6599ee5079fe4e7a882d6d8a61d3c572ef26f
Inevera11 <zuzannabozek2002@gmail.com>
2022-12-30 12:15:36 +0100

    ReactBrowserRouter

---
+++ src/data/FetchPokemonData.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Pokemon } from "../types/interfaces";
+const fetchData = async (url: string) => {
+  const response = await fetch(url);
+  if (response.status !== 200) {
+    throw new Error("cannot fetch the data");
+  }
+  const data = await response.json();
+  return data;
+};
+
+const FetchPokemonData = async () => {
+  const { results: allPokemonsData } = await fetchData(
+    "https://pokeapi.co/api/v2/pokemon?limit=200&offset=0"
+  );
+
+  const arrayOfPokemons: Pokemon[] = [];
+  for await (const pokemon of allPokemonsData) {
+    const { id, name, types } = await fetchData(pokemon.url);
+    const newPokemon = {
+      id,
+      name,
+      img:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+        id +
+        ".png",
+      types: types.map(
+        (pokemonType: { type: { name: string } }) => pokemonType.type.name
+      ),
+    };
+    arrayOfPokemons.push(newPokemon);
+  }
+  return arrayOfPokemons;
+};
+
+export default FetchPokemonData;
--- src/index.tsx
+++ src/index.tsx
@@ -9,51 +9,22 @@ import {
 } from "react-router-dom";
 import MyCards from "./Routes/MyCards";
 import { Pokemon } from "./types/interfaces";
+import Home from "./Routes/Home";
+import FetchPokemonData from "./data/FetchPokemonData";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-
-const fetchData = async (url: string) => {
-  const response = await fetch(url);
-  if (response.status !== 200) {
-    throw new Error("cannot fetch the data");
-  }
-  const data = await response.json();
-  return data;
-};
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
+      { path: "/", element: <Home /> },
       {
         path: "/myCards",
         element: <MyCards />,
-        loader: async () => {
-          const { results: allPokemonsData } = await fetchData(
-            "https://pokeapi.co/api/v2/pokemon?limit=200&offset=0"
-          );
-
-          const arrayOfPokemons: Pokemon[] = [];
-          for await (const pokemon of allPokemonsData) {
-            const { id, name, types } = await fetchData(pokemon.url);
-            const newPokemon = {
-              id,
-              name,
-              img:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
-                id +
-                ".png",
-              types: types.map(
-                (pokemonType: { type: { name: string } }) =>
-                  pokemonType.type.name
-              ),
-            };
-            arrayOfPokemons.push(newPokemon);
-          }
-          return arrayOfPokemons;
-        },
+        loader: FetchPokemonData,
       },
     ],
   },
